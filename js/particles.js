dc37f2a3946ef187430966dfb51fe37dc031ce04
Chris Amaya <amayarivasc@gmail.com>
2021-07-18 22:58:26 -0500

    ADD: slide project added

--- js/particles.js
+++ js/particles.js
@@ -54,6 +54,34 @@ const initParticles = () => {
       parallaxMultiplier: 20, // Lower the number is more extreme parallax
       particleRadius: 2, // Dot size
     });
+
+    particleground(document.getElementById('particles-foreground-slide2'), {
+      dotColor: 'rgba(255, 255, 255, 1)',
+      lineColor: 'rgba(255, 255, 255, 0.05)',
+      minSpeedX: 0.3,
+      maxSpeedX: 0.6,
+      minSpeedY: 0.3,
+      maxSpeedY: 0.6,
+      density: 50000, // One particle every n pixels
+      curvedLines: false,
+      proximity: 250, // How close two dots need to be before they join
+      parallaxMultiplier: 10, // Lower the number is more extreme parallax
+      particleRadius: 4, // Dot size
+    });
+        
+    particleground(document.getElementById('particles-background-slide2'), {
+      dotColor: 'rgba(255, 255, 255, 1)',
+      lineColor: 'rgba(255, 255, 255, 0.05)',
+      minSpeedX: 0.075,
+      maxSpeedX: 0.15,
+      minSpeedY: 0.075,
+      maxSpeedY: 0.15,
+      density: 30000, // One particle every n pixels
+      curvedLines: false,
+      proximity: 20, // How close two dots need to be before they join
+      parallaxMultiplier: 20, // Lower the number is more extreme parallax
+      particleRadius: 2, // Dot size
+    });
       
     particleground(document.getElementById('particles-foreground-slide3'), {
       dotColor: 'rgba(255, 255, 255, 1)',
